64999a542f4361d977f2ac57b935ecaf4a8f6d7c
Abhi gautam <151918728+abhideveloper9753@users.noreply.github.com>
2026-02-17 17:39:10 +0530

    Update CORS origin to include production URL

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.post(
 
 
 app.use(cors(
-    {origin:"http://localhost:5173",
+    {origin:"https://examnotesclient.onrender.com",
         credentials:true,
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
     }
